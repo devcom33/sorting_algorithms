f21c771759590ff1030dee07f0471a9530cdf264
devcom33 <mouad.elb06@gmail.com>
2024-01-01 12:02:18 -0800

    try

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j + 1] < array[j])
 			{
