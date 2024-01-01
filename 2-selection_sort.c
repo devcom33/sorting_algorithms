91b029a834b0482bb0f336011d4061b13f6f03f1
devcom33 <mouad.elb06@gmail.com>
2024-01-01 12:48:39 -0800

    selction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,16 +13,19 @@ void selection_sort(int *array, size_t size)
 	{
 		min_index = i;
 
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size ; j++)
 		{
 			if (array[j] < array[min_index])
 			{
 				min_index = j;
 			}
 		}
-		temp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = temp;
-		print_array(array, size);
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
     	}
 }
