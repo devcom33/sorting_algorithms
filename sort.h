9eb4b9d0d030d0a84ca86fbe2adf110593f0e996
devcom33 <mouad.elb06@gmail.com>
2024-01-01 13:37:34 -0800

    100

--- sort.h
+++ sort.h
@@ -16,17 +16,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*======================================================================*/
-/*=================         Printing Algoritms        ==================*/
-/*======================================================================*/
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-
-/*======================================================================*/
-/*=================          sorting Algoritms        ==================*/
-/*======================================================================*/
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
